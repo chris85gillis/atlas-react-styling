c906d31d94bddd039ef76c511cc68f58e74ce957
Chris Gillis <chris85gillis@gmail.com>
2024-09-09 16:36:55 -0500

    Task 6, cleaned up and made images worked with deployment

--- src/components/CoverArt.jsx
+++ src/components/CoverArt.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import placeholder from '../assets/placeholder.svg';
 
 const CoverArt = () => {
   return (
     <div className="flex justify-left items-center">
       <img
-        src="/src/assets/placeholder.svg"
+        src={placeholder}
         alt="Cover Art"
         className="w-84 h-84 object-cover rounded-lg shadow-lg"
       />
